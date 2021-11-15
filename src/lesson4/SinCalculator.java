4f8faf2ef68b4f3de7275e99a97cad50c0ae577b
Oleksii Usenko <lokk153@gmail.com>
2021-11-15 19:40:08 +0200

    update lesson4.SinCalculator

--- src/lesson4/SinCalculator.java
+++ src/lesson4/SinCalculator.java
@@ -3,7 +3,7 @@ package lesson4;
 public class SinCalculator {
     public static void main(String[] args) {
         System.out.println("degrees | sin");
-        for (double degrees = 0; degrees <= 360; degrees = degrees + 10) {
+        for (double degrees = 0; degrees <= 360; degrees +=10) {
             System.out.format("%.0f | %.4f%n", degrees, Math.sin(Math.toRadians(degrees)));
             System.out.println("-------------");
         }
